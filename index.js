08dddb26adc198bbc6ed72d3a893f68214a832d3
Dieguidev <diegogaraycullas@gmail.com>
2023-03-06 17:26:05 -0500

    abriendo el puerto de la api

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.get("/", (req, res) =>{
+  res.send("Hola mi server en Express");
+});
+
+app.listen(PORT, () =>{
+  console.log("My port: " + PORT);
+});
