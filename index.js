870cfd5bcda9ff27fdea705c36fa2f12e40c5965
Dieguidev <diegogaraycullas@gmail.com>
2023-03-06 18:17:04 -0500

    implementando faker

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) =>{
-  res.send("Hola mi server en Express");
+const { faker } = require('@faker-js/faker');
+
+// app.get("/", (req, res) =>{
+//   res.send("Hola mi server en Express");
+// });
+
+app.get("/products", (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+
+  }
+  res.json(products);
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
   console.log("My port: " + PORT);
 });
