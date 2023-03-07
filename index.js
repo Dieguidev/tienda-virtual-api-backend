3b4a7df860ae0be1e4dc2898df19d81c9367e003
Dieguidev <diegogaraycullas@gmail.com>
2023-03-06 19:13:49 -0500

    creanbdo rutas

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const routerApi = require('./routes/index.router')
 
-const { faker } = require('@faker-js/faker');
+app.use(express.json());
 
-// app.get("/", (req, res) =>{
-//   res.send("Hola mi server en Express");
-// });
-
-app.get("/products", (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    })
-
-  }
-  res.json(products);
-});
+routerApi(app);
 
 app.listen(PORT, () => {
   console.log("My port: " + PORT);
---
+++ routes/index.router.js
@@ -0,0 +1,11 @@
+const productsRouter = require('./products.routes.js');
+const express = require('express');
+const router = express.Router();
+
+function routerApi(app) {
+  app.use('/api/v1',router);
+  router.use('/products', productsRouter);
+}
+
+
+module.exports = routerApi;
