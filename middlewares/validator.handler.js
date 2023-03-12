2d089d004d690690edb318b269b4fb73dd440a4a
Dieguidev <diegogaraycullas@gmail.com>
2023-03-11 19:40:49 -0500

    actualizando joi

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,9 +1,9 @@
-const boom = require ('@hapi/boom');
+const boom = require('@hapi/boom');
 
 function validatorHandler(schema, property) {
   return (req, res, next) => {
     const data = req[property];
-    const { error } = schema.validate(data);
+    const { error } = schema.validate(data, { abortEarly: false });  //abortEraly hace que el Joi envie todos los errores
     if (error) {
       next(boom.badRequest(error));
     }
