952e8eb8635a54b600a06a9fc32519cdcfc1aacc
Dieguidev <diegogaraycullas@gmail.com>
2023-03-11 19:04:56 -0500

    implmentando middleware de error

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const { logErrors, errorHandles,boomErrorHandler } = require('./middlewares/error.handler');
 const app = express();
 const PORT = 3000;
-const routerApi = require('./routes/index.router')
+const routerApi = require('./routes/index.router');
 
 app.use(express.json());
 
 routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandles);
 
 app.listen(PORT, () => {
   console.log("My port: " + PORT);
