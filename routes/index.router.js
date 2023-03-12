ca2c6c27b73585fa2fc5485cec74f3cd7ff2c81a
Dieguidev <diegogaraycullas@gmail.com>
2023-03-12 18:14:24 -0500

    implementando modelo customers

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,11 +2,13 @@ const productsRouter = require('./products.routes.js');
 const express = require('express');
 const router = express.Router();
 const usersRouter = require('./users.router');
+const customersRouter = require('./customers.router')
 
 function routerApi(app) {
   app.use('/api/v1',router);
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
+  router.use('/customers',customersRouter);
 }
 
 
