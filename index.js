fa5112ace867dd23690c9797a3bb775476bb32e0
Dieguidev <diegogaraycullas@gmail.com>
2023-03-11 20:23:32 -0500

    aplicando cors

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const { logErrors, errorHandles,boomErrorHandler } = require('./middlewares/error.handler');
+const cors = require('cors')
+const { logErrors, errorHandles, boomErrorHandler } = require('./middlewares/error.handler');
 const app = express();
 const PORT = 3000;
 const routerApi = require('./routes/index.router');
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors());
 
 routerApi(app);
 app.use(logErrors);
