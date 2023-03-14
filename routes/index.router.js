cd18efdba8e7e40f55476e37ab2a9a74e484f3b0
Dieguidev <diegogaraycullas@gmail.com>
2023-03-13 21:18:37 -0500

    creando modelos y migraciones de products y categries

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,13 +2,15 @@ const productsRouter = require('./products.routes.js');
 const express = require('express');
 const router = express.Router();
 const usersRouter = require('./users.router');
-const customersRouter = require('./customers.router')
+const customersRouter = require('./customers.router');
+const categoriesRouter = require('./categories.router')
 
 function routerApi(app) {
   app.use('/api/v1',router);
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/customers',customersRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 
